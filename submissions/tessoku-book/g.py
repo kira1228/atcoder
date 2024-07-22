617833934ad78bdbdd76b14d98d8bca638142de2
吉原輝人 <kiratoyoshihara@gmail.com>
2024-07-22 15:48:01 +0900

    add submission: 2024/07/22 15:48:01

--- submissions/tessoku-book/g.py
+++ submissions/tessoku-book/g.py
@@ -10,30 +10,13 @@
 10
 11
 12
-13
-14
-15
-16
-17
-18
-19
-20
-# 入力（前半）
-N = int(input())
-X = [ None ] * N
-Y = [ None ] * N
-for i in range(N):
-  X[i], Y[i] = map(int, input().split())
-# 入力（後半）
-Q = int(input())
-A = [ None ] * Q
-B = [ None ] * Q
-C = [ None ] * Q
-D = [ None ] * Q
-for i in range(Q):
-  A[i], B[i], C[i], D[i] = map(int, input().split())
-# 各座標にある点の数を数える
-S = [ [ 0 ] * 1501 for i in range(1501) ]
-for i in range(N):
-  S[X[i]][Y[i]] += 1
- 
+N, K = map(int, input().split())
+S = str(input())
+ON = 0
+for i in range(len(S)):
+    if S[i] == "1":
+        ON += 1
+if abs(ON - K) % 2 == 0:
+    print("Yes")
+else:
+    print("No")
--- submissions/tessoku-book/i.py
+++ submissions/tessoku-book/i.py
@@ -10,30 +10,13 @@
 10
 11
 12
-13
-14
-15
-16
-17
-18
-19
-20
-# 入力
-H, W, N = map(int, input().split())
-A = [ None ] * N
-B = [ None ] * N
-C = [ None ] * N
-D = [ None ] * N
-# X = [ None ] * (W)
-for t in range(N):
-  A[t], B[t], C[t], D[t] = map(int, input().split())
-# 各日について加算
-X = [ [ 0 ] * (W + 2) for i in range(H + 2) ]
-Z = [ [ 0 ] * (W + 2) for i in range(H + 2) ]
-for t in range(N):
-    X[A[t]][B[t]] += 1
-    X[A[t]][D[t]+1] -= 1
-    X[C[t]+1][B[t]] -= 1
-    X[C[t]+1][D[t]+1] += 1
-# 二次元累積和をとる
- 
+N, K = map(int, input().split())
+S = str(input())
+ON = 0
+for i in range(len(S)):
+    if S[i] == "1":
+        ON += 1
+if abs(ON - K) % 2 == 0:
+    print("Yes")
+else:
+    print("No")
